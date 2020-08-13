1ac30bce307c98960735f0f7a4fde5d53281ef13
Alban Crequy <alban@kinvolk.io>
2020-08-13 12:18:31 +0200

    mirror minikube artifacts on S3 too

--- 00-Getting_Started/start_minikube.sh
+++ 00-Getting_Started/start_minikube.sh
@@ -5,10 +5,12 @@ set -e
 # Download the executable and the ISO
 echo Downloading minikube components
 if [[ ! -f minikube ]]; then
-  wget https://eleven.dev.kinvolk.io/u/alban/cloud-native-bpf-workshop/minikube
+  URL=https://2020-08-17-cloud-native-bpf-workshop-public.s3.eu-central-1.amazonaws.com/minikube
+  wget $URL
 fi
 if [[ ! -f minikube.iso ]]; then
-  wget https://eleven.dev.kinvolk.io/u/alban/cloud-native-bpf-workshop/minikube.iso
+  URL=https://2020-08-17-cloud-native-bpf-workshop-public.s3.eu-central-1.amazonaws.com/minikube.iso
+  wget $URL
 fi
 
 # Verify that the sha256sums are correct
@@ -18,7 +20,17 @@ sha256sum -c <<EOF
 645c7943a793d30d4d7ea34767a184741f8aece9860a22f9ba2d264825530dfc  minikube.iso
 EOF
 
+echo This script will delete the existing minikube VM if you have one, and
+echo start a new one.
+read -p "Are you sure? [yes/no] " -r
+
+if [[ ! $REPLY =~ ^yes$ ]]; then
+  echo Abort.
+  exit 1
+fi
+
 echo Executing minikube
 chmod +x minikube
 ./minikube delete
 ./minikube start --driver=kvm2 --iso-url=file://$(pwd)/minikube.iso
+./minikube ssh -- 'bash -c "uname -a ; if [ -e /sys/kernel/kheaders.tar.xz ] ; then echo kheaders.tar.xz available ; else echo kheaders.tar.xz missing ; fi"'
